64649d95780793f275b81667b04c51302d9097c0
buju4e <bojan.petrovski@student.um.si>
2024-05-06 00:38:18 +0200

    feat: Tracking of production models

--- backend/IE-app/scripts/test_model.py
+++ backend/IE-app/scripts/test_model.py
@@ -10,6 +10,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 import mlflow
 import mlflow.tensorflow
 from dotenv import load_dotenv
+from mlflow.tracking import MlflowClient
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,6 +18,8 @@ load_dotenv()
 # Setup MLflow to track to DagsHub
 mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
 mlflow.set_experiment('Bike_Sharing_Demand_Forecasting')
+
+client = MlflowClient()
 
 def create_sequences(data, n_steps):
     X, y = [], []
@@ -59,7 +62,7 @@ def main():
     n_steps = 3  # Static look-back period
     X_train, X_test, y_train, y_test = preprocess_and_load_data(train_file, test_file, n_steps)
 
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         model = build_lstm_model(n_steps, X_train.shape[2])
         history = model.fit(X_train, y_train, epochs=30, validation_data=(X_test, y_test), verbose=2, callbacks=[EarlyStopping(monitor='val_loss', patience=10)])
         
@@ -75,8 +78,20 @@ def main():
         mlflow.log_metric("train_r2", r2_score(y_train, y_train_pred))
         mlflow.log_metric("test_r2", r2_score(y_test, y_test_pred))
 
-        # Save and log the model within the MLflow
-        mlflow.keras.log_model(model, "model")
+        # Register and evaluate the model
+        model_uri = mlflow.keras.log_model(model, "model", registered_model_name="Bike_Sharing_Demand_Forecasting")
+        latest_prod_model = client.get_latest_versions("Bike_Sharing_Demand_Forecasting", stages=["Production"])
+        
+        if latest_prod_model:
+            best_mse = latest_prod_model[0].metrics['test_mse']
+            if history.history['val_loss'][-1] < best_mse:
+                client.transition_model_version_stage(
+                    name="Bike_Sharing_Demand_Forecasting",
+                    version=run.info.run_id,
+                    stage="Production",
+                    archive_existing_versions=True
+                )
+
         print("Model training and testing complete. Metrics reported.")
 
 if __name__ == "__main__":
